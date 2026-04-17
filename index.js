7b8ab1ea928a79aad5e8cf23b4cfc5a04af556ef
Diego Mamede <diegomamede244@gmail.com>
2026-04-17 12:01:54 -0300

    Adicionar página HTML para gerenciar vídeos draft da Panda Video

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Olá, mundo! Este é o projeto Node.js inicializado.');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Servir arquivos estáticos da pasta atual
+app.use(express.static(path.join(__dirname)));
+
+// Rota para a página principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
